5580a14f4671a68ce191ed247b6655392d67d331
Marcelo Chaves <9marceli@gmail.com>
2022-09-20 14:31:03 -0300

    Adding functions to change tasks, filter and organize code for future maintenance

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,6 +1,6 @@
 <template>
   <Box>
-    <div class="columns">
+    <div class="columns clicavel" @click="tarefaClicada">
       <div class="column is-4">
         {{ tarefa.descricao || 'Tarefa sem descrição' }}
       </div>
@@ -15,13 +15,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 import Cronometro from "./Cronometro.vue";
 import Box from "./Box.vue";
 import ITarefa from "../interfaces/ITarefa";
 
 export default defineComponent({
   name: 'Tarefa',
+  emits: ['aoTarefaClicada'],
   components: {
     Cronometro,
     Box
@@ -32,12 +33,24 @@ export default defineComponent({
       required: true
     }
   },
-  computed: {
-    tempoGasto () : string {
-      return new Date(this.tarefa.duracaoEmSegundos * 1000)
+  setup(props, { emit }) {
+    const tarefaClicada = () : void => {
+      emit('aoTarefaClicada', props.tarefa)
+    }
+    const tempoGasto = computed(() => {
+      return new Date(props.tarefa.duracaoEmSegundos * 1000)
         .toISOString()
         .substr(11, 8)
+    })
+    return {
+      tarefaClicada,
+      tempoGasto
     }
   }
 });
 </script>
+<style scoped>
+.clicavel {
+  cursor: pointer;
+}
+</style>
